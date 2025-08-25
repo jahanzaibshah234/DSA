3e754ed06fed8d4a3ff5841af8802a35b87e186e
Jahanzaib Khalid <154453607+jahanzaibshah234@users.noreply.github.com>
2025-08-25 14:43:02 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove all occurrences of a character in a string/remove-all-occurrences-of-a-character-in-a-string.py
@@ -0,0 +1,6 @@
+#User function Template for python3
+class Solution:
+    # Function to remove all occurrences of the character from the string
+    def removeCharacter(self, s, c):
+        # code here
+        return s.replace(c, "")
