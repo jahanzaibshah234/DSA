3ed31372b46dbb3138974d610ddc35aa10559a8d
Jahanzaib Khalid <154453607+jahanzaibshah234@users.noreply.github.com>
2025-08-29 12:14:47 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse a String/reverse-a-string.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+
+class Solution:
+     def reverseString(self, s: str) -> str:
+        # code here
+        reversed_string = s[::-1]
+        return reversed_string
