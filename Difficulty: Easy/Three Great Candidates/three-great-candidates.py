18c97e066f7d95a295e04c7d78c3264b7d9da912
Jahanzaib Khalid <154453607+jahanzaibshah234@users.noreply.github.com>
2025-09-06 14:02:22 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Three Great Candidates/three-great-candidates.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+
+class Solution:
+    def maxProduct(self, arr):
+        # code here
+        arr.sort()
+        return max(arr[0]*arr[1]*arr[-1], arr[-1]*arr[-2]*arr[-3])
